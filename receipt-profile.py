e8f036158f3a8d76ba5ca0f7a8ba4358f3493cf8
DolicaAkelloEgwel <d.akelloegwel@lcc.arts.ac.uk>
2024-11-25 17:09:02 +0000

    tidying

--- receipt-profile.py
+++ receipt-profile.py
@@ -1,20 +1,32 @@
 import base64
 import json
 
+import pytesseract
 import requests
 import torch
+import yaml
 from diffusers import StableDiffusionPipeline
+from PIL import Image
 
+# Read Config File
+with open("config.yaml", "r") as file:
+    config = yaml.safe_load(file)
+
+# Path to Tesseract
+pytesseract.pytesseract.tesseract_cmd = config["tesseract_path"]
+
+# Ollama Setup
 OLLAMA_API_URL = "http://localhost:11434/api/generate"
-IMG_TO_TXT_MODEL = "moondream"
-GENERATE_PROFILE_MODEL = "dolphin-phi"
 
-model_id = "OFA-Sys/small-stable-diffusion-v0"
-pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
+# Stable Diffusion Setup
+MODEL_ID = "OFA-Sys/small-stable-diffusion-v0"
+pipe = StableDiffusionPipeline.from_pretrained(MODEL_ID, torch_dtype=torch.float16)
 pipe = pipe.to("cuda")
 
-with open("aldi-receipt.jpg", "rb") as image_file:
-    encoded_string = base64.b64encode(image_file.read()).decode("utf-8")
+# Read the text from the image
+image = Image.open(config["input_image"])
+extracted_text = pytesseract.image_to_string(image)
+print("Receipt contents:", extracted_text)
 
 
 def talk_to_ollama(url, data):
@@ -32,33 +44,17 @@ def talk_to_ollama(url, data):
         return "Error: " + str(response.status_code) + response.text
 
 
-img_to_txt_data = {
-    "model": IMG_TO_TXT_MODEL,
-    "prompt": "What are the items on this receipt?",
-    "stream": False,
-    "images": [encoded_string],
-}
-
-response = talk_to_ollama(OLLAMA_API_URL, img_to_txt_data)
-if "Error" in response:
-    print("Unable to generate image description:", response)
-    exit()
-
-print("Receipt contents:", response)
-
 generate_profile_data = {
-    "model": GENERATE_PROFILE_MODEL,
-    "prompt": f"Here are the purchases: {response}. The imagined sex, race, height, style of dress, age range of such a person is...",
+    "model": config["ollama_model"],
+    "prompt": f"Here s the text from a receipt: {extracted_text}. Given what is on this receipt, can you please GUESS the sex, race, height, style of dress, age range, etc of the customer. You guess does not need to be accurate. You are simply making an assumption about the *type* of person who might buy such things.",
     "stream": False,
 }
-response = talk_to_ollama(OLLAMA_API_URL, generate_profile_data)
-print(response)
 
 response = talk_to_ollama(OLLAMA_API_URL, generate_profile_data)
 if "Error" in response:
     print("Unable to generate customer description", response)
     exit()
+print(response)
 
 image = pipe(response + " 4K").images[0]
-
 image.save("output.png")
