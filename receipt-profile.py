956940f67b7342d352fcefc0d92525b606e41ebc
DolicaAkelloEgwel <d.akelloegwel@lcc.arts.ac.uk>
2024-11-25 17:17:03 +0000

    url in config because why knows

--- receipt-profile.py
+++ receipt-profile.py
@@ -15,15 +15,12 @@ with open("config.yaml", "r") as file:
 # Path to Tesseract
 pytesseract.pytesseract.tesseract_cmd = config["tesseract_path"]
 
-# Ollama Setup
-OLLAMA_API_URL = "http://localhost:11434/api/generate"
-
 # Stable Diffusion Setup
 MODEL_ID = "OFA-Sys/small-stable-diffusion-v0"
 pipe = StableDiffusionPipeline.from_pretrained(MODEL_ID, torch_dtype=torch.float16)
 pipe = pipe.to("cuda")
 
-# Read the text from the image
+# Read the text from the receipt image
 image = Image.open(config["input_image"])
 extracted_text = pytesseract.image_to_string(image)
 print("Receipt contents:", extracted_text)
@@ -43,18 +40,19 @@ def talk_to_ollama(url, data):
     else:
         return "Error: " + str(response.status_code) + response.text
 
-
+# Get Ollama to analyse the customer
 generate_profile_data = {
     "model": config["ollama_model"],
     "prompt": f"Here s the text from a receipt: {extracted_text}. Given what is on this receipt, can you please GUESS the sex, race, height, style of dress, age range, etc of the customer. You guess does not need to be accurate. You are simply making an assumption about the *type* of person who might buy such things.",
     "stream": False,
 }
 
-response = talk_to_ollama(OLLAMA_API_URL, generate_profile_data)
+response = talk_to_ollama(config["ollama_url"], generate_profile_data)
 if "Error" in response:
     print("Unable to generate customer description", response)
     exit()
 print(response)
 
+# Create a picture
 image = pipe(response + " 4K").images[0]
 image.save("output.png")
