3eae540a211974269efaa26d960492bebf3ad460
DolicaAkelloEgwel <d.akelloegwel@lcc.arts.ac.uk>
2024-11-28 18:00:36 +0000

    well...

--- main.py
+++ main.py
@@ -25,35 +25,6 @@ image = Image.open(FULL_IMAGE_PATH)
 extracted_text = pytesseract.image_to_string(image)
 print("Receipt contents:", extracted_text)
 
-
-def talk_to_ollama(url, data):
-
-    response = requests.post(
-        url, headers={"Content-Type": "application/json"}, data=json.dumps(data)
-    )
-
-    if response.status_code == 200:
-        response_text = response.text
-        data = json.loads(response_text)
-        actual_response = data["response"]
-        return actual_response
-    else:
-        return "Error: " + str(response.status_code) + response.text
-
-
-# Get Ollama to analyse the customer
-generate_profile_data = {
-    "model": config["ollama_model"],
-    "prompt": f"Here s the text from a receipt: {extracted_text}. Given what is on this receipt, can you please GUESS the sex, race, height, style of dress, age range, etc of the customer. You guess does not need to be accurate. You are simply making an assumption about the *type* of person who might buy such things.",
-    "stream": False,
-}
-
-response = talk_to_ollama(config["ollama_url"], generate_profile_data)
-if "Error" in response:
-    print("Unable to generate customer description", response)
-    exit()
-print(response)
-
 if platform.system() == "Windows":
     from create_picture import create_picture
 
@@ -62,5 +33,5 @@ if platform.system() == "Windows":
 else:
     import base64
 
-    response = requests.post("http://localhost:8000/txt2img", json={"prompt": response})
+    response = requests.post("http://localhost:8000/txt2img", json={"receipt_content": extracted_text})
     print(response.json())
--- txttoimg_api.py
+++ txttoimg_api.py
@@ -7,11 +7,25 @@ from create_picture import create_picture
 
 app = Flask(__name__)
 
+pipe = pipeline("text-generation", model="TinyLlama/TinyLlama-1.1B-Chat-v1.0", torch_dtype=torch.bfloat16, device_map="auto")
 
 @app.route("/txt2img", methods=["POST"])
 def create_image():
     data = request.get_json()
-    prompt = data.get("prompt")
+    receipt_content = data.get("receipt_content")
+
+    messages = [
+    {
+        "role": "system",
+        "content": "You are a friendly chatbot who makes judgements about people based on their purchases. You can look at a receipt and guess a person's sex, age range, style of dress, hairstyle, level of body modification, etc.",
+    },
+    {"role": "user", "content": f"Please describe the profile of a person with the following purchses: {receipt_content}"},
+    ]
+    prompt = pipe.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
+    outputs = pipe(prompt, max_new_tokens=256, do_sample=True, temperature=0.7, top_k=50, top_p=0.95)
+    prompt = outputs[0]["generated_text"]
+
+
     img = create_picture(prompt)
 
     im_file = BytesIO()
